5e4d54f49cc462c72ffff3fc057af639d3ba2cda
Graeme Paterson <ghpaterson@gmail.com>
2023-02-21 14:34:26 +0000

    renderDailyWeather

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
 
 function renderWeather({ current, daily, hourly }) {
   renderCurrentWeather(current);
-  // renderDailyWeather(daily);
+  renderDailyWeather(daily);
   // renderHourlyWeather(hourly);
   document.body.classList.remove("blurred");
 }
@@ -34,4 +34,18 @@ function renderCurrentWeather(current) {
   setValue("current-fl-low", current.lowFeelsLike);
   setValue("current-wind", current.windSpeed);
   setValue("current-precip", current.precip);
+}
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "short" });
+const dailySection = document.querySelector("[data-day-section]");
+const dayCardTemplate = document.getElementById("day-card-template");
+function renderDailyWeather(daily) {
+  dailySection.innerHTML = "";
+  daily.forEach((day) => {
+    const element = dayCardTemplate.content.cloneNode(true);
+    setValue("temp", day.maxTemp, { parent: element });
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode);
+    dailySection.append(element);
+  });
 }
